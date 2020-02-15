0e2ddf399032b275c161a47805fad3df9f59df5b
deepan <deepanchakravarthi434@gmail.com>
2020-02-15 14:20:53 +0530

    Commenting the code

--- commands/movie.js
+++ commands/movie.js
@@ -1,15 +1,19 @@
+//IMPORTS
 const config = require('../config.json');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const {
   RichEmbed
 } = require("discord.js");
+//IMPORTS
 
+//CODE TO EXECUTE 
 module.exports = {
   name: "movie",
   description: "information about the requested movie",
   execute(message, args) {
-    const movieUrl = `http://www.omdbapi.com/?apikey=${config.movieAPI}&t=${args}&r=json`;
+    const movieUrl = `http://www.omdbapi.com/?apikey=${config.movieAPI}&t=${args}&r=json`; //Movie (api) url
 
+    //get movie details from url using xmlhttpsreq
     function httpGet(theUrl) {
       try {
         var xmlHttp = null;
@@ -19,12 +23,17 @@ module.exports = {
         xmlHttp.send(null);
         var movieDetailsText = xmlHttp.responseText;
         var movieDetailsJson = JSON.parse(movieDetailsText);
-        console.log(movieDetailsJson);
-        const urlRegex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gi;
+        //get movie details from url using xmlhttpsreq
 
+        //REGEX to check if url is valid
+        const urlRegex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gi;
+        //REGEX to check if url is valid
+
+        //Function to test REGEX
         function urlChecker(url) {
           return urlRegex.test(url) ? url : 'https://github.com/Darkcodelab/film-buddy/blob/master/images/imagenotfound.png?raw=true';
         }
+        //Function to test REGEX
 
         if (movieDetailsJson.Response == 'False') {
           message.channel.send('Error');
@@ -35,21 +44,21 @@ module.exports = {
             .setTitle(movieDetailsJson.Title)
             // .setThumbnail(urlChecker(movieDetailsJson.Poster))
             .setDescription(`**Released** : ${movieDetailsJson.Released}\n 
-                        **Rated**: ${movieDetailsJson.Rated}\n
-                        **Runtime**: ${movieDetailsJson.Runtime}\n
-                        **Genre**: ${movieDetailsJson.Genre}\n
-                        **Director**: ${movieDetailsJson.Director}\n
-                        **Actors**: ${movieDetailsJson.Actors}\n
-                        **Language**: ${movieDetailsJson.Language}\n
-                        **Awards**: ${movieDetailsJson.Awards}\n
-                        **Plot**: ${movieDetailsJson.Plot}\n
-                        **IMDB Rating**: ${movieDetailsJson.imdbRating}\n
-                        **Meta Score**: ${movieDetailsJson.Metascore}\n
-                        **IMDB ID**: ${movieDetailsJson.imdbID}\n
-                        **Box Office**: ${movieDetailsJson.BoxOffice}\n
-                        **Production**: ${movieDetailsJson.Production}
-
-        `)
+          **Rated**: ${movieDetailsJson.Rated}\n
+          **Runtime**: ${movieDetailsJson.Runtime}\n
+          **Genre**: ${movieDetailsJson.Genre}\n
+          **Director**: ${movieDetailsJson.Director}\n
+          **Actors**: ${movieDetailsJson.Actors}\n
+          **Language**: ${movieDetailsJson.Language}\n
+          **Awards**: ${movieDetailsJson.Awards}\n
+          **Plot**: ${movieDetailsJson.Plot}\n
+          **IMDB Rating**: ${movieDetailsJson.imdbRating}\n
+          **Meta Score**: ${movieDetailsJson.Metascore}\n
+          **IMDB ID**: ${movieDetailsJson.imdbID}\n
+          **Box Office**: ${movieDetailsJson.BoxOffice}\n
+          **Production**: ${movieDetailsJson.Production}
+          
+          `)
             .setImage(urlChecker(movieDetailsJson.Poster))
             .setTimestamp()
           message.channel.send(movieDetailsEmbed);
@@ -67,3 +76,4 @@ module.exports = {
     httpGet(movieUrl);
   }
 };
+//CODE TO EXECUTE 
